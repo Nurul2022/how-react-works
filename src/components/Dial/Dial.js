5e11e0f37fb9905b3b0a87c7b5ba9715452ccfa1
Nurul Islam <nurulbdpersonal@gmail.com>
2022-03-21 15:11:38 +0600

    dial added

---
+++ src/components/Dial/Dial.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dial = () => {
+    return (
+        <div style={{border: '2px solid blue', margin: '5px'}}>
+            <h3>This is Dial</h3>
+            <p>Steps so far: </p>
+        </div>
+    );
+};
+
+export default Dial;

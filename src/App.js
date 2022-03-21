6b5fce61a02757988516aa63e087519a26ce5602
Nurul Islam <nurulbdpersonal@gmail.com>
2022-03-21 16:23:29 +0600

    final code added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Device from './components/Device/Device';
 import Watch from './components/Watch/Watch';
+import Tablet from './components/Tablet/Tablet';
 
 function App() {
   return (
     <div className="App">
       <Device name='Uphone' price='55000'></Device>
       <Watch></Watch>
+      
     </div>
   );
 }
